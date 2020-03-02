12832ab14bca5aec59b250d65e48cc11eaf51095
Branford Shaw <branford.shaw@gmail.com>
2020-03-02 16:36:31 -0500

    used standard function (as opposed to arrow) to scrape article titles from buzzfeed using axios

--- controller/controller.js
+++ controller/controller.js
@@ -1,14 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const axios = require('axios');
 
 const cheerio = require('cheerio');
 
-const comment = require('../models/comment.js');
-const article = require('../models/article.js');
+const Comment = require('../models/Comment.js');
+const Article = require('../models/Article.js');
 
 router.get('/', (req, res) => {
 	res.render('index');
+});
+
+router.get('/scrape', function(req, res) {
+	// First, we grab the body of the html with request
+	axios.get('https://www.buzzfeednews.com').then(function(response) {
+		// Then, we load that into cheerio and save it to $ for a shorthand selector
+		var $ = cheerio.load(response.data);
+
+		// Now, we grab every h2 within an article tag, and do the following:
+		$('article.newsblock-story-card').each(function(i, element) {
+			// Save an empty result object
+			const result = {};
+
+			// Add the text and href of every link, and save them as properties of the result object
+
+			result.title = $(element)
+				.children('span.newsblock-story-card__info')
+				.children('h2')
+				.children('a')
+				.text();
+
+			result.link = $(element)
+				.children('span.newsblock-story-card__info')
+				.children('h2')
+				.children('a');
+
+			console.log(result);
+			// Create a new Article using the `result` object built from scraping
+			// db.Article.create(result)
+			// 	.then(function(dbArticle) {
+			// 		// View the added result in the console
+			// 		console.log(dbArticle);
+			// 	})
+			// 	.catch(function(err) {
+			// 		// If an error occurred, send it to the client
+			// 		return res.json(err);
+			// 	});
+		});
+
+		// If we were able to successfully scrape and save an Article, send a message to the client
+		res.send('Scrape Complete');
+	});
 });
 
 module.exports = router;
